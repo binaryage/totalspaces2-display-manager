a1754a56ed8cb26fe91963e030af22a0cb47318b
Stephen Sykes <sdsykes@gmail.com>
2014-04-19 17:12:39 +0300

    Initial Commit

---
+++ DisplaySpacesManager/BAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BAAppDelegate.h
+//  DisplaySpacesManager
+//
+//  Created by Stephen Sykes on 19/04/14.
+//  Copyright (c) 2014 Binaryage. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BAAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ DisplaySpacesManager/BAAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BAAppDelegate.m
+//  DisplaySpacesManager
+//
+//  Created by Stephen Sykes on 19/04/14.
+//  Copyright (c) 2014 Binaryage. All rights reserved.
+//
+
+#import "BAAppDelegate.h"
+
+@implementation BAAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
